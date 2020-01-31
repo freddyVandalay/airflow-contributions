297eb60ce693415f4d297422b4870a562fa351c1
Fredrik.Sundstrom <fsund001@gmail.com>
2020-01-31 10:40:17 +0000

    rm airlfow headings and added todo

--- operators/emr_create_jobflow_operator_override.py
+++ operators/emr_create_jobflow_operator_override.py
@@ -1,21 +1,4 @@
 # -*- coding: utf-8 -*-
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
 from airflow.contrib.operators.emr_create_job_flow_operator import EmrCreateJobFlowOperator
 from airflow.utils import apply_defaults
 from airflow.hooks import S3_hook
@@ -28,7 +11,9 @@ import os
 class EmrCreateJobFlowOperatorOverride(EmrCreateJobFlowOperator):
     """
     This operator overrides the default EmrCreateJobFlowOperator
+    see https://github.com/apache/airflow/blob/master/airflow/contrib/operators/emr_create_job_flow_operator.py
     TODO update custom variables
+    TODO update description and param 
 
     Bootstrap:
         - by default picks bootstrap from local airflow repo (emr/bootstrap.sh). Pass var to use custom path.
